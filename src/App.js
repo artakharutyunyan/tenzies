2db4e4c9cf88399b688af74e4071a9530dc8c947
Artak Harutyunyan <artak_harutyunyan@mail.com>
2022-02-12 20:13:30 +0400

    Game logic (WIP)

--- src/App.js
+++ src/App.js
@@ -1,11 +1,36 @@
+import React, { useState } from 'react';
 import Die from './Die';
+import { nanoid } from 'nanoid';
 
 function App() {
+  const [dice, setDice] = useState(allNewDice());
+
+  function allNewDice() {
+    const newDice = [];
+    for (let i = 0; i < 10; i++) {
+      newDice.push({
+        value: Math.ceil(Math.random() * 6),
+        isHeld: false,
+        id: nanoid(),
+      });
+    }
+    return newDice;
+  }
+
+  function rollDice() {
+    setDice(allNewDice());
+  }
+
+  const diceElements = dice.map((die) => (
+    <Die value={die.value} key={die.id} isHeld={die.isHeld} />
+  ));
+
   return (
     <main>
-      <div className="dice-container">
-        <Die value="1" />
-      </div>
+      <div className="dice-container">{diceElements}</div>
+      <button className="roll-dice" onClick={rollDice}>
+        Roll
+      </button>
     </main>
   );
 }
--- src/Die.js
+++ src/Die.js
@@ -1,8 +1,11 @@
 import React from 'react';
 
 export default function Die(props) {
+  const styles = {
+    backgroundColor: props.isHeld ? '#59e391' : 'white',
+  };
   return (
-    <div className="die-face">
+    <div className="die-face" style={styles}>
       <h2 className="die-num">{props.value}</h2>
     </div>
   );
